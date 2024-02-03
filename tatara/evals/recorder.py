0521b4db2dc33da1e20f799f72cbe132814fd997
Will Seltzer <1661264+willseltzer@users.noreply.github.com>
2024-02-03 11:26:51 -0800

    move tatara recorder for circular import

--- tatara/evals/recorder.py
+++ tatara/evals/recorder.py
@@ -2,9 +2,7 @@ import atexit
 import json
 from abc import ABC, abstractmethod
 
-from tatara.evals.eval_types import RecordWithMultipleEvalResults, EvalRun
-from tatara.network._tatara_network_client import TataraNetworkClient
-from tatara.tatara import _get_network_client
+from tatara.evals.eval_types import RecordWithMultipleEvalResults
 
 MIN_FLUSH_EVENTS = 100
 MIN_FLUSH_SECONDS = 10
@@ -49,24 +47,4 @@ class FileRecorder(RecorderBase):
                 f.write(json.dumps(eval_row.to_dict()) + "\n")
         self.results = []
 
-
-class TataraRecorder(RecorderBase):
-    def __init__(self):
-        super().__init__()
-        self.eval_runs = []
-        self.total_rows = 0
-        self.tatara_network_client: TataraNetworkClient = _get_network_client()
-
-
-    def flush_events(self):
-        for eval_run in self.eval_runs:
-            self.tatara_network_client.send_eval_run_post_request(eval_run)
-        self.eval_runs = []
-    
-
-    def record_eval_run(self, eval_run: EvalRun):
-        if self.total_rows > MIN_FLUSH_EVENTS:
-            self.flush_events()
-        self.eval_runs.append(eval_run)
-        self.total_rows += eval_run.num_rows
 
---
+++ tatara/evals/tatara_recorder.py
@@ -0,0 +1,25 @@
+from .recorder import RecorderBase, MIN_FLUSH_EVENTS
+from tatara.network._tatara_network_client import TataraNetworkClient
+from tatara.tatara import _get_network_client
+from tatara.evals.eval_types import EvalRun
+
+class TataraRecorder(RecorderBase):
+    def __init__(self):
+        super().__init__()
+        self.eval_runs = []
+        self.total_rows = 0
+        self.tatara_network_client: TataraNetworkClient = _get_network_client()
+
+
+    def flush_events(self):
+        for eval_run in self.eval_runs:
+            self.tatara_network_client.send_eval_run_post_request(eval_run)
+        self.eval_runs = []
+    
+
+    def record_eval_run(self, eval_run: EvalRun):
+        if self.total_rows > MIN_FLUSH_EVENTS:
+            self.flush_events()
+        self.eval_runs.append(eval_run)
+        self.total_rows += eval_run.num_rows
+
