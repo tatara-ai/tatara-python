2d46fe3c9da14bff0d9223ca971d3c9d109ba0b6
Will Seltzer <1661264+willseltzer@users.noreply.github.com>
2024-02-07 12:16:46 -0800

    add get_dataset from tatara

--- tatara/__init__.py
+++ tatara/__init__.py
@@ -1,6 +1,6 @@
 from .evals import Eval
 from .evals.record import Record
-from .dataset import init_dataset, run_evals
+from .dataset import init_dataset, run_evals, get_dataset
 
 
-__all__ = ['Eval', 'Record', 'init_dataset', 'run_evals']
+__all__ = ['Eval', 'Record', 'init_dataset', 'run_evals', 'get_dataset']
