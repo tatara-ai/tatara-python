05ac9b243ffdd21f71959e9e22916509c93371a6
Will Seltzer <1661264+willseltzer@users.noreply.github.com>
2024-02-10 13:32:29 -0800

    import tatara types from tatara

--- tatara/__init__.py
+++ tatara/__init__.py
@@ -1,6 +1,7 @@
 from .evals import Eval
 from .evals.record import Record
+from .tatara_types import  LLMParams, LLMUsageMetrics, DiffusionParams, DiffusionPrompt
 from .dataset import init_dataset, run_evals, get_dataset
 
 
-__all__ = ['Eval', 'Record', 'init_dataset', 'run_evals', 'get_dataset']
+__all__ = ['Eval', 'Record', 'init_dataset', 'run_evals', 'get_dataset', 'LLMParams', 'LLMUsageMetrics', 'DiffusionParams', 'DiffusionPrompt']
